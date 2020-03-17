a04cf665c0f6295a26d97f83ee2c44706282e3f4
Roman <roman@ga-games.com>
2020-03-17 19:15:39 +0200

    package.json

--- publish.sh
+++ publish.sh
@@ -4,24 +4,24 @@
 yarn build
 
 # remove the old stuff
-rm -rf ~/vue-atlaskit-npm
+rm -rf vue-atlaskit-npm
 
 #
-mkdir ~/vue-atlaskit-npm
+mkdir vue-atlaskit-npm
 
 # copy the important stuff
-cp -r src ~/vue-atlaskit-npm
-cp -r dist ~/vue-atlaskit-npm
+cp -r src vue-atlaskit-npm
+cp -r dist vue-atlaskit-npm
 
 # copy over package spec, license and readme
-cp package.json ~/vue-atlaskit-npm
-cp LICENSE ~/vue-atlaskit-npm
-cp README.md ~/vue-atlaskit-npm
+cp package.json vue-atlaskit-npm
+cp LICENSE vue-atlaskit-npm
+cp README.md vue-atlaskit-npm
 
 # remove the example component
-rm ~/vue-atlaskit-npm/src/App.vue
+rm vue-atlaskit-npm/src/App.vue
 
 # remove the generated demo.html
-rm ~/vue-atlaskit-npm/dist/demo.html
+rm vue-atlaskit-npm/dist/demo.html
 
 exit 0
